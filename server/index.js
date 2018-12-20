d3a222c5a3aac18e5c2d5510adec8b71983ecd1b
Jonathan Wang <jonathancharles66@gmail.com>
2018-12-20 12:40:55 -0800

    set up express server get express (ticket "set up express server get request")

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const db = require('./db/index.js');
+
 let app = express();
 app.set('port', 3000);
 app.use(express.static('client/dist'));
+
+app.get('/api/turash/description/:id', function(req, res) {
+  var id = req.params.id;
+  var queryString = `select * from carInfo where id = ${id};`
+  db.query(queryString, function(err, result) {
+    if (err) { throw err; }
+    else {
+      res.send(result[0]);
+    }
+  });
+  // db.getData(function(data) {
+  //   res.send(data);
+  // });
+})
+
 app.listen(app.get('port'), function() {
   console.log(`Listening on port `, app.get('port'));
 });
---
+++ server/test.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql');
+const request = require('reqest');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
