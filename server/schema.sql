bf143cac3058a7fa5999bc2ba12df80c8f6b445f
Morgan Grace <morgangrace@Morgans-iMac.local>
2019-01-09 23:11:07 -0800

    seeding database with 65000 records

--- server/schema.sql
+++ server/schema.sql
@@ -1,26 +1,23 @@
 DROP DATABASE IF EXISTS cars;
 CREATE DATABASE cars;
 
-USE cars;
 
-CREATE TABLE carInfo (
-  id int NOT NULL AUTO_INCREMENT,
-  companyName varchar(20) NOT NULL,
-  carName varchar(50) NOT NULL,
-  edition varchar(50) NOT NULL,
-  trips int NOT NULL,
-  mpg int NOT NULL,
-  gas varchar(20) NOT NULL,
-  doors int NOT NULL,
-  seats int NOT NULL,
-  description varchar(1000) NOT NULL,
-  business varchar(50) NOT NULL,
-  features varchar(30) NOT NULL,
-  extras varchar(1000) NOT NULL,
-  guidelines varchar(5000) NOT NULL,
-  faq varchar(5000) NOT NULL,
-  moreD varchar(1000) NOT NULL,
-  moreE varchar(1000) NOT NULL,
-  sentence varchar(100) NOT NULL,
-  PRIMARY KEY (id)
-);
+for
+(i = 1; i < 10000; i += 1) {
+
+//   const queryArgs = [ faker.random.number({ min: 10, max: 100 }), faker.random.number({ min: 16, max: 30 }), `Gas (${faker.random.arrayElement(['Premium', 'Regular'])})`, 4, faker.random.arrayElement
+([2, 4, 5, 6, 7]), faker.lorem.paragraph
+(), 'This host caters to business travelers.', 'Automatic transmission', faker.lorem.paragraph
+(), faker.lorem.paragraph
+(), faker.lorem.paragraph
+(), faker.lorem.paragraph
+(), faker.lorem.paragraph
+(), faker.lorem.sentence
+(8)];
+//   connection.query
+(queryString, queryArgs,
+(err) => {
+//
+if (err) { throw err; }
+//   });
+// }
